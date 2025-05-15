67c9d3e0c9fc50ae745f701f2fffde9bc6455a06
M1rmaduke <89578467+M1rmaduke@users.noreply.github.com>
2025-05-15 23:17:33 +0200

    Add files via upload

---
+++ qrscanner.js
@@ -0,0 +1,11 @@
+﻿console.log("QR scanner script loaded");
+
+window.addEventListener('DOMContentLoaded', function () {
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader", { fps: 10, qrbox: 250 });
+
+    html5QrcodeScanner.render(function (decodedText, decodedResult) {
+        alert("QR-kode læst: " + decodedText);
+        html5QrcodeScanner.clear();
+    });
+});
